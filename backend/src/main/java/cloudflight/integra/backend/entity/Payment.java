ce0e9c69b9c29d7ecc8c3c1bf9c0ea29a275f7d1
Daria <dambeandaria@gmail.com>
2025-09-24 19:04:25 +0300

    Fix OneToOne mapping for Payment -> Expense

--- backend/src/main/java/cloudflight/integra/backend/entity/Payment.java
+++ backend/src/main/java/cloudflight/integra/backend/entity/Payment.java
@@ -1,14 +1,5 @@
 package cloudflight.integra.backend.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.EnumType;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -22,8 +13,8 @@ public class Payment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "expense_id", nullable = false) //asigură că fiecare Payment trebuie să aibă un Expense
+    @OneToOne(optional = false)
+    @JoinColumn(name = "expense_id", nullable = false) // Asigură că fiecare Payment trebuie să aibă un Expense
     private Expense expense;
 
     @Column(nullable = false)
