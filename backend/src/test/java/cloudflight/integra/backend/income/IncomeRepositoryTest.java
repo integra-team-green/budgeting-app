b72ef5d146de2859ab0d3ca95bcf5ca6768aefb5
Andrei0921 <rotaruandrei904@gmail.com>
2025-09-25 23:07:40 +0300

    #20 DB Income: using h2 database for tests

--- backend/src/test/java/cloudflight/integra/backend/income/IncomeRepositoryTest.java
+++ backend/src/test/java/cloudflight/integra/backend/income/IncomeRepositoryTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -19,6 +20,7 @@ import java.util.stream.StreamSupport;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IncomeRepositoryTest {
 
     @Autowired
--- backend/src/test/java/cloudflight/integra/backend/income/IncomeServiceTest.java
+++ backend/src/test/java/cloudflight/integra/backend/income/IncomeServiceTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -26,6 +27,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IncomeServiceTest {
     @Autowired
     private IncomeServiceImpl service;
