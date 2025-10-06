e145fcf6aa23045aedfd3e8127d8bd5cb7e9c6f6
Emilian Cuciurean <ceir3511@scs.ubbcluj.ro>
2025-10-07 00:27:33 +0300

    #42 finished login page, register page, and i think first user dashboard

--- frontend/src/app/component/login/login.component.ts
+++ frontend/src/app/component/login/login.component.ts
@@ -3,26 +3,13 @@ import {AuthService} from '../../services/auth.service';
 import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {Router, RouterModule} from '@angular/router';
-import {InputTextModule} from 'primeng/inputtext';
-import {PasswordModule} from 'primeng/password';
-import {ButtonModule} from 'primeng/button';
-import {MessageModule} from 'primeng/message';
-import {Checkbox} from 'primeng/checkbox';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
   standalone: true,
-  imports: [
-    FormsModule,
-    CommonModule,
-    RouterModule,
-    InputTextModule,
-    PasswordModule,
-    ButtonModule,
-    MessageModule
-  ]
+  imports: [FormsModule, CommonModule, RouterModule]
 })
 export class LoginComponent {
   username = '';
@@ -65,6 +52,7 @@ export class LoginComponent {
 
         console.error('Login error:', err);
       }
+
     });
   }
 }
